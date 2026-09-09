7e615f8e5e8bdf1239fde799d0cbe6de791a2374
Testermt <mjm548526@gmail.com>
2026-09-09 22:08:53 +0530

    nrw 33333334

--- js/member.js
+++ js/member.js
@@ -3,11 +3,23 @@
 
 // ==================== GEOFENCING CONFIGURATION ====================
 // ==================== GEOFENCING CONFIGURATION ====================
+/*
 const GYM_LOCATION = {
   lat: 29.456545,           // Tera ghar/testing latitude
   lng: 77.717185,           // Tera ghar/testing longitude
   allowedRadiusMeters: 40   // Range in meters
 };
+*/
+
+const GYM_LOCATION = {
+  lat: 29.456923,     // Tera ghar/testing latitude
+  lng: 77.717848,           // Tera ghar/testing longitude
+  allowedRadiusMeters: 40   // Range in meters
+};
+
+
+
+
 
 
 // Haversine formula to calculate distance in meters
