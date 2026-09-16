0583dc98d29b8f7c21158af4f438ee102653c95d
Testermt <mjm548526@gmail.com>
2026-09-16 17:01:20 +0530

    new chart

--- js/member.js
+++ js/member.js
@@ -460,9 +460,7 @@ async function performAutoCheckin(member) {
     }
 
     await checkinsCol.doc(docId).set({
-    	
       memberId: member.id,
-      name: member.name, //
       phone: member.phone,
       dateKey: todayKey,
       method: "manual",
